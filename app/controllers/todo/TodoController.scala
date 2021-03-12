ce0600a0a6d6ee1d9743cdb89b40578679d446a4
ruka.kanno <ruka.kanno@nextbeat.net>
2021-03-12 17:49:01 +0900

    ゴミ箱ページを追加

--- app/controllers/todo/TodoController.scala
+++ app/controllers/todo/TodoController.scala
@@ -29,10 +29,17 @@ class TodoController @Inject()(val controllerComponents: ControllerComponents) e
         jsSrc  = Seq("main.js")
     )
 
-    def list() = Action.async { implicit request => 
+    def list_page() = Action.async { implicit request => 
         TodoRepository().getAll.map { value => 
             val todo_list_vv = ViewValueList(vv, RegisterFormData.registerForm, SelectIdFormData.selectIdForm, value.map(_.v))
             Ok(views.html.todo.List(todo_list_vv))
+        }
+    }
+
+    def trush_page() = Action.async { implicit request => 
+        TodoRepository().getAll.map { value => 
+            val todo_list_vv = ViewValueList(vv.copy(title = "ゴミ箱"), RegisterFormData.registerForm, SelectIdFormData.selectIdForm, value.map(_.v))
+            Ok(views.html.todo.Trush(todo_list_vv))
         }
     }
 
@@ -48,29 +55,41 @@ class TodoController @Inject()(val controllerComponents: ControllerComponents) e
             (formData: RegisterFormData) => {
                 val todo = Todo(0, formData.title, formData.body, 0)
                 TodoRepository().add(todo).map { _ =>
-                    Redirect("/todo")
+                    Redirect(controllers.todo.routes.TodoController.list_page())
                 }
             }
         )
     }
 
-    def delete() = Action.async { implicit request =>
+    private def trush(success: SelectIdFormData => Future[_]) = Action.async { implicit request =>
         SelectIdFormData.selectIdForm.bindFromRequest().fold (
-            (formWithErrors: Form[SelectIdFormData]) => Future.successful(Redirect("/todo")),
+            (formWithErrors: Form[SelectIdFormData]) => Future.successful(Redirect(controllers.todo.routes.TodoController.trush_page())),
             (formData: SelectIdFormData) => {
-                TodoRepository().removeAll(formData.ids.map(Todo.Id(_))).map { _ =>
-                    Redirect("/todo")
+                success(formData).map { _ =>
+                    Redirect(controllers.todo.routes.TodoController.trush_page())
                 }
             }
         )
     }
+
+    def archive() = trush(formData => {
+        TodoRepository().archiveAll(formData.ids.map(Todo.Id(_)))
+    })
+
+    def unarchive() = trush(formData => {
+        TodoRepository().unarchiveAll(formData.ids.map(Todo.Id(_)))
+    })
+
+    def delete() = trush(formData => {
+        TodoRepository().removeAll(formData.ids.map(Todo.Id(_)))
+    })
 
     def update() = Action.async { implicit request =>
         SelectIdFormData.selectIdForm.bindFromRequest().fold (
-            (formWithErrors: Form[SelectIdFormData]) => Future.successful(Redirect("/todo")),
+            (formWithErrors: Form[SelectIdFormData]) => Future.successful(Redirect(controllers.todo.routes.TodoController.list_page())),
             (formData: SelectIdFormData) => {
                 TodoRepository().toggleStateAll(formData.ids.map(Todo.Id(_))).map { _ =>
-                    Redirect("/todo")
+                    Redirect(controllers.todo.routes.TodoController.list_page())
                 }
             }
         )
