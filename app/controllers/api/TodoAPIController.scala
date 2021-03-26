f4c0bc0d17c65c2318085a32a9dc59ae282b480a
ruka.kanno <ruka.kanno@nextbeat.net>
2021-03-26 15:14:01 +0900

    todoを返すjsonを定義

--- app/controllers/api/TodoAPIController.scala
+++ app/controllers/api/TodoAPIController.scala
@@ -5,14 +5,26 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.mvc._
 import javax.inject._
-import lib.persistence.onMySQL.driver
+import lib.persistence.default._
+import lib.model.Todo
+import controllers.json.TodoWrites
 
 @Singleton
 class TodoAPIController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  case class TodosJson (
+    todo: Seq[TodoWrites]
+  )
+  object TodosJson {
+    implicit val writes = Json.writes[TodosJson]
+  }
+
   def todos = Action.async { implicit request =>
-    
-    Future.successful(
-      Ok(Json.obj("status" -> "ok"))
-    )
+    for {
+      todoList: Seq[Todo.EmbeddedId] <- TodoRepository.getAll
+    } yield {
+      val todosJson = TodosJson(todoList.map(TodoWrites.createTodoWrites))
+      Ok(Json.toJson(todosJson))
+    }
   }
 }
